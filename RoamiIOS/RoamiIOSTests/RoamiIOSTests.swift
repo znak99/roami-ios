23ce60ca88f14efc81abf385b6b585bb0aaa45fb
znak99 <xhahekcldhkznak0113@gmail.com>
2025-04-09 05:12:04 +0900

    프로젝트 초기화

---
+++ RoamiIOS/RoamiIOSTests/RoamiIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoamiIOSTests.swift
+//  RoamiIOSTests
+//
+//  Created by seungwoo on 2025/04/09.
+//
+
+import Testing
+@testable import RoamiIOS
+
+struct RoamiIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
