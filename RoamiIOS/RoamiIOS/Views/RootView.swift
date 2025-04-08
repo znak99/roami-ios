583389ea273864643f67cfe24f8b480f67cab560
znak99 <xhahekcldhkznak0113@gmail.com>
2025-04-09 05:22:57 +0900

    프로젝트 디렉토리 설정

---
+++ RoamiIOS/RoamiIOS/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  RoamiIOS
+//
+//  Created by seungwoo on 2025/04/09.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
