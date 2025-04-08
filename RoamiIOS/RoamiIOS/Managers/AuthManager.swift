fb1b1c9d0dd62de0d323d6eed28d17d9c0651808
znak99 <xhahekcldhkznak0113@gmail.com>
2025-04-09 05:31:18 +0900

    AuthManager 파일 추가

---
+++ RoamiIOS/RoamiIOS/Managers/AuthManager.swift
@@ -0,0 +1,12 @@
+//
+//  AuthManager.swift
+//  RoamiIOS
+//
+//  Created by seungwoo on 2025/04/09.
+//
+
+import Foundation
+
+class AuthManager: ObservableObject {
+    @Published var isAuthenticated: Bool = false
+}
